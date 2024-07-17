477d86cd6bb0c1b92eb382bde11789a6eb473e55
Thisara <sasmithajayasinghe1@gmail.com>
2024-07-17 21:29:28 +0530

    card

--- src/PageComponents/HomeItems.tsx
+++ src/PageComponents/HomeItems.tsx
@@ -12,6 +12,7 @@ import homeImg2 from "../styles/home2.jpg";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "@/Redux/store";
 import { fetchPostByDistrict } from "@/Redux/Post/PostAction";
+import { TfiLocationPin } from "react-icons/tfi";
 
 export default function HomeItems() {
   const dispatch = useDispatch<AppDispatch>();
@@ -59,7 +60,7 @@ export default function HomeItems() {
                           </div>
                         ) : (
                           allPost.map((post) => (
-                            <article className="mb-4 overflow-hidden rounded-xl border text-gray-700 shadow-md duration-500 ease-in-out hover:shadow-xl">
+                            <article className="mb-4 w-[500px] max-w-full overflow-hidden rounded-xl border text-gray-700 shadow-md duration-500 ease-in-out hover:shadow-xl">
                               <div className="w-full h-48 overflow-hidden">
                                 <img
                                   src={post.images[0]}
@@ -69,43 +70,50 @@ export default function HomeItems() {
                               </div>
 
                               <div className="p-4">
-                                <div className="pb-6">
+                                <div className="pb-1">
                                   <a
                                     href="#"
                                     className="text-lg hover:text-green-600 font-medium duration-500 ease-in-out"
                                   >
-                                    {post.location}
+                                    {post.title}
                                   </a>
                                 </div>
-                                
+                                <div className="pb-6">
+                                  <a
+                                    href="#"
+                                    className="flex items-center text-sm font-medium duration-500 ease-in-out"
+                                  >
+                                    <TfiLocationPin className="mr-2" />
+                                    <span>{post.cityDistrict}</span>
+                                  </a>
+                                </div>
+                                <ul className="box-border flex flex-wrap list-none border-t border-b border-solid border-gray-200 px-0 py-6">
+                                  {post.facilities.includes("Cooking") && (
+                                    <li className="mr-6 flex items-center text-left">
+                                      <i className="mr-2 text-2xl text-green-600">
+                                        {/* Cooking icon SVG */}
+                                        <svg
+                                          xmlns="http://www.w3.org/2000/svg"
+                                          aria-hidden="true"
+                                          className="h-5 w-5"
+                                          preserveAspectRatio="xMidYMid meet"
+                                          viewBox="0 0 24 24"
+                                        >
+                                          <path
+                                            fill="currentColor"
+                                            d="M10.38 13.08A1 1 0 0 0 10 13H6a1 1 0 0 0 0 2h1.59l-5.3 5.29a1 1 0 0 0 0 1.42a1 1 0 0 0 1.42 0L9 16.41V18a1 1 0 0 0 2 0v-4a1 1 0 0 0-.08-.38a1 1 0 0 0-.54-.54ZM10 5a1 1 0 0 0-1 1v1.59l-5.29-5.3a1 1 0 0 0-1.42 1.42L7.59 9H6a1 1 0 0 0 0 2h4a1 1 0 0 0 .38-.08a1 1 0 0 0 .54-.54A1 1 0 0 0 11 10V6a1 1 0 0 0-1-1Zm3.62 5.92A1 1 0 0 0 14 11h4a1 1 0 0 0 0-2h-1.59l5.3-5.29a1 1 0 1 0-1.42-1.42L15 7.59V6a1 1 0 0 0-2 0v4a1 1 0 0 0 .08.38a1 1 0 0 0 .54.54ZM16.41 15H18a1 1 0 0 0 0-2h-4a1 1 0 0 0-.38.08a1 1 0 0 0-.54.54A1 1 0 0 0 13 14v4a1 1 0 0 0 2 0v-1.59l5.29 5.3a1 1 0 0 0 1.42 0a1 1 0 0 0 0-1.42Z"
+                                          />
+                                        </svg>
+                                      </i>
+                                      <span className="text-sm">Cooking</span>
+                                    </li>
+                                  )}
 
-                                <ul className="box-border flex list-none items-center border-t border-b border-solid border-gray-200 px-0 py-6">
-                                    {post.facilities.includes('Cooking') &&(
-                                        <li className="mr-4 flex items-center text-left">
-                                    <i className="mr-2 text-2xl text-green-600">
-                                      {/* <!-- uil:compress-arrows --> */}
-                                      <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        aria-hidden="true"
-                                        className="h-5 w-5"
-                                        preserveAspectRatio="xMidYMid meet"
-                                        viewBox="0 0 24 24"
-                                      >
-                                        <path
-                                          fill="currentColor"
-                                          d="M10.38 13.08A1 1 0 0 0 10 13H6a1 1 0 0 0 0 2h1.59l-5.3 5.29a1 1 0 0 0 0 1.42a1 1 0 0 0 1.42 0L9 16.41V18a1 1 0 0 0 2 0v-4a1 1 0 0 0-.08-.38a1 1 0 0 0-.54-.54ZM10 5a1 1 0 0 0-1 1v1.59l-5.29-5.3a1 1 0 0 0-1.42 1.42L7.59 9H6a1 1 0 0 0 0 2h4a1 1 0 0 0 .38-.08a1 1 0 0 0 .54-.54A1 1 0 0 0 11 10V6a1 1 0 0 0-1-1Zm3.62 5.92A1 1 0 0 0 14 11h4a1 1 0 0 0 0-2h-1.59l5.3-5.29a1 1 0 1 0-1.42-1.42L15 7.59V6a1 1 0 0 0-2 0v4a1 1 0 0 0 .08.38a1 1 0 0 0 .54.54ZM16.41 15H18a1 1 0 0 0 0-2h-4a1 1 0 0 0-.38.08a1 1 0 0 0-.54.54A1 1 0 0 0 13 14v4a1 1 0 0 0 2 0v-1.59l5.29 5.3a1 1 0 0 0 1.42 0a1 1 0 0 0 0-1.42Z"
-                                        />
-                                      </svg>
-                                    </i>
-                                    <span className="text-sm">1200sqf</span>
-                                  </li>
-
-                                    )}
                                   
 
-                                  <li className="mr-4 flex items-center text-left">
+                                  <li className="mr-6 flex items-center text-left">
                                     <i className="mr-2 text-2xl text-green-600">
-                                      {/* <!-- ic:outline-king-bed --> */}
+                                      {/* Beds icon SVG */}
                                       <svg
                                         xmlns="http://www.w3.org/2000/svg"
                                         aria-hidden="true"
@@ -119,12 +127,11 @@ export default function HomeItems() {
                                         />
                                       </svg>
                                     </i>
-                                    <span className="text-sm">4 Beds</span>
+                                    <span className="text-sm">4 Baths</span>
                                   </li>
-
-                                  <li className="flex items-center text-left">
+                                  <li className="mr-6 flex items-center text-left">
                                     <i className="mr-2 text-2xl text-green-600">
-                                      {/* <!-- bx:bath --> */}
+                                      {/* Baths icon SVG */}
                                       <svg
                                         xmlns="http://www.w3.org/2000/svg"
                                         aria-hidden="true"
