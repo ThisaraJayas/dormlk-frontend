b040146ecde346f90c8a6fa04eb22be01b43b6d4
Thisara <sasmithajayasinghe1@gmail.com>
2024-08-12 20:05:02 +0530

    changes

--- src/Redux/Auth/AuthAction.ts
+++ src/Redux/Auth/AuthAction.ts
@@ -15,7 +15,7 @@ export const register = createAsyncThunk("register", async (userData: Register) 
     console.log(userData);
     
     try {
-        const { data } = await axios.post(`http://localhost:8080/auth/register`, userData);
+        const { data } = await axios.post(`https://dormlk-production.up.railway.app/auth/register`, userData);
         console.log(data);
 
         if (data.jwt) {
@@ -32,7 +32,7 @@ export const login = createAsyncThunk("login", async(userData : Login)=>{
     console.log(userData);
     
     try{
-        const {data} = await axios.post(`http://localhost:8080/auth/login`,userData)
+        const {data} = await axios.post(`https://dormlk-production.up.railway.app/auth/login`,userData)
         if(data.jwt){
             localStorage.setItem("jwt",data.jwt)
         }
@@ -49,7 +49,7 @@ export const getUser = createAsyncThunk("getUser", async()=>{
         const jwt = localStorage.getItem("jwt");
         if (!jwt) throw new Error("JWT not found");
 
-        const { data } = await axios.get(`http://localhost:8080/api/user/profile`, {
+        const { data } = await axios.get(`https://dormlk-production.up.railway.app/api/user/profile`, {
             headers: {
                 "Authorization": `Bearer ${jwt}`
             }
@@ -68,7 +68,7 @@ export const passwordChange = createAsyncThunk("passwordChange", async(newPasswo
     try{
         const jwt = localStorage.getItem("jwt");
         if (!jwt) throw new Error("JWT not found");
-        const {data} = await axios.put(`http://localhost:8080/api/user/change-password/${newPassword}`,
+        const {data} = await axios.put(`https://dormlk-production.up.railway.app/api/user/change-password/${newPassword}`,
             { }, 
             {
               headers: {
