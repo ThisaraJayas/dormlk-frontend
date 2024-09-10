4bf3df288d7baed9d44118c6355e559a9bf81abc
Thisara <sasmithajayasinghe1@gmail.com>
2024-09-10 16:53:30 +0530

    change

--- src/PageComponents/HomeTitle.tsx
+++ src/PageComponents/HomeTitle.tsx
@@ -5,7 +5,7 @@ export default function HomeTitle() {
     <section className="relative overflow-hidden py-12 sm:py-16 lg:py-10">
     <div className="mx-auto px-8 sm:px-12 lg:px-16">
     <h1 className="text-3xl font-bold font-sans">Find Housing Fast and Easy</h1>
-    <p className="mt-5 text-gray-500">The United States of America (USA), commonly known as the United States or America, is a country comprising 50 states, a federal district, five major self-governing territories, and various possessions. At 3.8 million square miles, it is the world's third or fourth-largest country by total area and is slightly smaller than the entire continent of Europe.  </p>
+    <p className="mt-5 text-gray-500">Discover the perfect boarding place in minutes! With Dorm.lk, browse a wide range of accommodations, filter by location and price, and connect directly with property owners. Simplifying your housing search, one click at a time. </p>
 
     </div>
     </section>
