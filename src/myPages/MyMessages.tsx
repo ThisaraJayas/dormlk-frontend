2cde712515d4f2d2cd83f16e93a13e6d8a0f8f1c
Thisara <sasmithajayasinghe1@gmail.com>
2024-09-10 11:55:28 +0530

    dd

--- src/myPages/MyMessages.tsx
+++ src/myPages/MyMessages.tsx
@@ -59,6 +59,8 @@ export default function MyProfile() {
 
   // Received Messages: Messages where the post owner is the logged-in user but the message author is someone else
   const receivedMessages = messagesRecived.filter(message => message.post.user._id === loginUser?._id && message.user._id !== loginUser?._id);
+  const postMessage = allMessage.filter(message => message.post.user._id === loginUser?._id);
+console.log("RECIVED MESSAFE ",postMessage);
 
   // Sort messages and replies by date in descending order
   const sortedMyMessages = [...myMessages].sort((a, b) => new Date(b.createdDateTime).getTime() - new Date(a.createdDateTime).getTime());
